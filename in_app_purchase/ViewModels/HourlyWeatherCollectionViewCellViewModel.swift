39604ab3d8d048d465e6f151d76cc161b88f6826
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-10-19 23:51:25 +0200

    add cell constraints

--- in_app_purchase/ViewModels/HourlyWeatherCollectionViewCellViewModel.swift
+++ in_app_purchase/ViewModels/HourlyWeatherCollectionViewCellViewModel.swift
@@ -11,7 +11,26 @@ import WeatherKit
 struct HourlyWeatherCollectionViewCellViewModel {
     private let model: HourWeather
     
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        
+        return formatter
+    }()
+    
     init(model: HourWeather) {
         self.model = model
+    }
+    
+    public var iconName: String {
+        return model.symbolName
+    }
+    
+    public var temperature: String {
+        return model.temperature.description
+    }
+    
+    public var date: String {
+        let hour = Calendar.current.component(.hour, from: model.date)
+        return "\(hour):00"
     }
 }
--- in_app_purchase/Views/WeatherCells/CurrentWeatherCollectionViewCell.swift
+++ in_app_purchase/Views/WeatherCells/CurrentWeatherCollectionViewCell.swift
@@ -10,16 +10,73 @@ import UIKit
 final class CurrentWeatherCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: CurrentWeatherCollectionViewCell.self)
     
+    private let tempLabel: UILabel = {
+        let tempLabel = UILabel()
+        tempLabel.textAlignment = .center
+        tempLabel.font = .systemFont(ofSize: 62, weight: .medium)
+        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        return tempLabel
+    }()
+    
+    private let conditionLabel: UILabel = {
+        let conditionLabel = UILabel()
+        conditionLabel.textAlignment = .left
+        conditionLabel.font = .systemFont(ofSize: 64, weight: .regular)
+        conditionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return conditionLabel
+    }()
+    
+    private let icon: UIImageView = {
+        let icon = UIImageView()
+        icon.contentMode = .scaleAspectFit
+        icon.tintColor = .systemYellow
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .systemPurple
+        
+        contentView.addSubview(tempLabel)
+        contentView.addSubview(icon)
+        contentView.addSubview(conditionLabel)
+        
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        tempLabel.text = nil
+        conditionLabel.text = nil
+        icon.image = nil
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tempLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            tempLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            tempLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            
+            icon.heightAnchor.constraint(equalToConstant: 55),
+            icon.widthAnchor.constraint(equalToConstant: 55),
+            icon.topAnchor.constraint(equalTo: tempLabel.bottomAnchor),
+            icon.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            
+            conditionLabel.leftAnchor.constraint(equalTo: icon.rightAnchor, constant: 10),
+            conditionLabel.topAnchor.constraint(equalTo: tempLabel.bottomAnchor),
+            conditionLabel.heightAnchor.constraint(equalToConstant: 80),
+            conditionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 15),
+            conditionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+        ])
+    }
+    
     public func configure(with viewModel: CurrentWeatherCollectionViewCellViewModel) {
-        
+        tempLabel.text = viewModel.temperature
+        icon.image = UIImage(systemName: viewModel.symbolName)
+        conditionLabel.text = viewModel.condition
     }
 }
