bfb3b75975fbc9e752e008cddbf5dab2962b8dd5
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-10-26 23:08:37 +0200

    display paywall

--- in_app_purchase/Controllers/WeatherViewController.swift
+++ in_app_purchase/Controllers/WeatherViewController.swift
@@ -8,6 +8,7 @@
 import RevenueCat
 import RevenueCatUI
 import UIKit
+import WeatherKit
 
 class WeatherViewController: UIViewController {
     
@@ -17,11 +18,7 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         setUpView()
         getLocation()
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"),
-                                                            style: .done,
-                                                            target: self,
-                                                            action: #selector(didDapUpgrade))
+
     }
     
     @objc
@@ -36,11 +33,35 @@ class WeatherViewController: UIViewController {
             WeatherManager.shared.getWeather(for: location) { [weak self] in
                 DispatchQueue.main.async {
                     guard let currentWeather = WeatherManager.shared.currentWeather else { return }
-                    self?.primaryView.configure(with: [
-                        .current(viewModel: .init(model: currentWeather)),
-                        .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0) })),
-                        .daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0) }))
-                    ])
+                    self?.createViewModels(currentWeather: currentWeather)
+                }
+            }
+        }
+    }
+    
+    private func createViewModels(currentWeather: CurrentWeather) {
+        var viewModels: [WeatherViewModel] = [
+            .current(viewModel: .init(model: currentWeather)),
+            .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0) })),
+        ]
+        primaryView.configure(with: viewModels)
+        
+        IAPManager.shared.isSubscribed { [weak self] subscribed in
+            if subscribed {
+                viewModels.append(
+                    .daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0) }))
+                )
+                
+                DispatchQueue.main.async {
+                    self?.primaryView.configure(with: viewModels)
+                    self?.navigationItem.rightBarButtonItem = nil
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self?.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"),
+                                                                        style: .done,
+                                                                        target: self,
+                                                                        action: #selector(self?.didDapUpgrade))
                 }
             }
         }
@@ -61,13 +82,13 @@ class WeatherViewController: UIViewController {
 
 extension WeatherViewController: PaywallViewControllerDelegate {
     func paywallViewController(_ controller: PaywallViewController, didFinishPurchasingWith customerInfo: CustomerInfo) {
-        print("Purchased: \(customerInfo)")
+        controller.dismiss(animated: true)
+        guard let currentWeather = WeatherManager.shared.currentWeather else { return }
+        createViewModels(currentWeather: currentWeather)
     }
     
     func paywallViewController(_ controller: PaywallViewController, didFinishRestoringWith customerInfo: CustomerInfo) {
         print("Restored: \(customerInfo)")
+        controller.dismiss(animated: true)
     }
 }
-
-
-// 04:00
--- in_app_purchase/Managers/IAPManager.swift
+++ in_app_purchase/Managers/IAPManager.swift
@@ -6,11 +6,18 @@
 //
 
 import Foundation
+import RevenueCat
 
 final class IAPManager {
     static let shared = IAPManager()
     
-    private init() {
-        
+    private init() {}
+    
+    func isSubscribed(completion: @escaping (Bool) -> Void) {
+        Purchases.shared.getCustomerInfo { info, error in
+            guard let subscriptions = info?.activeSubscriptions else { return }
+            print("XXX: \(subscriptions)")
+            completion(!subscriptions.isEmpty)
+        }
     }
 }
