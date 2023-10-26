a53a2d0a5c4a1755c77b2a8611db572318729ce2
Damian Kieltyka <damian.kieltyka@icloud.com>
2023-10-26 23:10:24 +0200

    del

--- in_app_purchase/Other/AppDelegate.swift
+++ in_app_purchase/Other/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Purchases.logLevel = .debug
-        Purchases.configure(withAPIKey: "appl_MJzrYasXugHHloTcTnTaxOmXFae")
+        Purchases.configure(withAPIKey: "")
         return true
     }
 
